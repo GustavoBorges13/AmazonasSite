21ecbfbe8caa77d8afbede8450bf1573c2b6be19
GustavoBorges13 <ggustavo.borges13@gmail.com>
2024-11-18 11:46:12 -0300

    Update General

---
+++ script.js
@@ -0,0 +1,5 @@
+// Lógica para exibir/ocultar o submenu "2024.2"
+document.getElementById('menu-2024-2').addEventListener('click', function() {
+    var submenu = document.getElementById('submenu-2024-2');
+    submenu.classList.toggle('active');
+  });
